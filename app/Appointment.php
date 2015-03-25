c8b859a008b5406771d73a6cf98448c07d1c0821
Magic Mike <cardhousemagic@gmail.com>
2015-03-24 21:39:01 -0400

    Made times for appointments into Carbon

--- app/Appointment.php
+++ app/Appointment.php
@@ -6,6 +6,8 @@ class Appointment extends Model {
 
     protected $fillable = ['time'];
 
+    protected $dates = ['time'];
+
     public function dog()
     {
         return $this->belongsTo('Bubbles\Dog');
