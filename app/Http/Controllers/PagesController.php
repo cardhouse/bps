93435b8cb6be97bb785ccedbc87496dc973d6ec4
Magic Mike <cardhousemagic@gmail.com>
2015-03-25 00:38:44 -0400

    Added filters to the pages route

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,25 +5,44 @@ use Bubbles\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
+/**
+ * Class PagesController
+ *
+ * @Middleware("auth", only={"dashboard"})
+ * @Middleware("guest", only={"appointments"})
+ * @package Bubbles\Http\Controllers
+ */
 class PagesController extends Controller {
 
 
     function __construct()
     {
         $this->middleware('auth', ['only' => 'dashboard']);
+        $this->middleware('guest', ['only' => 'appointments']);
     }
 
+    /**
+     * @return \Illuminate\View\View
+     */
     public function home()
     {
         return view('pages.home');
 	}
 
+    /**
+     * @return \Illuminate\View\View
+     */
     public function dashboard()
     {
         $user = \Auth::user();
         return view('pages.dashboard', compact('user'));
     }
 
+
+    /**
+     *
+     * @return \Illuminate\View\View
+     */
     public function appointments()
     {
         return view('appointments.interstitial');
